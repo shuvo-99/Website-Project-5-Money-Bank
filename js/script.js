4b2f0dce06107fb4a27bf23075dfb870085f9a77
shuvo-99 <anontoshuvo99@gmail.com>
2022-02-17 20:42:16 +0600

    expense calc

--- js/script.js
+++ js/script.js
@@ -1,60 +1,24 @@
-function getInputValue(inputCost) {
-  const input = document.getElementById(inputCost);
+function getInputValue(inputParameter) {
+
+  const input = document.getElementById(inputParameter );
 
   const inputAmountText = input.value;
   const inputAmount = parseFloat(inputAmountText);
-  console.log(inputAmount);
-  // if (isNaN(inputAmount)){
-  // if (typeof(inputAmount)!='number'){
-
-  //     console.log('no')
-  //     const Integer = document.getElementById("notify-int");
-  //     Integer.style.display = "block";
-
-  // }
-  // else if(typeof(inputAmount)=='number'){
-
-  //     input.value = "";
-
-  //     return inputAmount;
-  // }
-  if (inputAmount >= 0) {
-    input.value = "";
-
-    return inputAmount;
-  } else {
-    console.log("no");
-    const positiveInt = document.getElementById("notify-positve-int");
-    positiveInt.style.display = "block";
-  }
+  console.log(inputAmount)
+  
   // input.value = "";
-
-  // return inputAmount;
+  
+  return inputAmount;
 }
 
-// function currentTotal() {
-//   const totalCost = document.getElementById("totalCost");
-//   const totalCostText = totalCost.innerText;
-//   const previousTotalCost = parseFloat(totalCostText);
-// }
 
 document.getElementById("calc-button").addEventListener("click", function () {
-  // const foodInput = document.getElementById('foodCost');
-  // const foodAmountText = foodInput.value;
-  // const foodAmount = parseFloat(foodAmountText)
-  // console.log(foodAmount);
+    
+  const incomeAmount = getInputValue("income");
+  
   const foodAmount = getInputValue("foodCost");
 
-  // const rentInput = document.getElementById('rentCost');
-  // const rentAmountText = rentInput.value;
-  // const rentAmount = parseFloat(rentAmountText)
-  // console.log(rentAmount);
   const rentAmount = getInputValue("rentCost");
-
-  // const clothesInput = document.getElementById('clothesCost');
-  // const clothesAmountText = clothesInput.value;
-  // const clothesAmount = parseFloat(clothesAmountText)
-  // console.log(clothesAmount);
 
   const clothesAmount = getInputValue("clothesCost");
 
