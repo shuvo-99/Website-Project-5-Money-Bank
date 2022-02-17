29ef59de76aab5b3c51642a3e959f72c6d53cfbe
shuvo-99 <anontoshuvo99@gmail.com>
2022-02-17 20:28:28 +0600

    rent cost added

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@ document.getElementById("calc-button").addEventListener("click", function () {
   const foodAmountText = foodInput.value;
   const foodAmount = parseFloat(foodAmountText);
   console.log(foodAmount);
+
+  const rentInput = document.getElementById("rentCost");
+  const rentAmountText = rentInput.value;
+  const rentAmount = parseFloat(rentAmountText);
+  console.log(rentAmount);
 });
